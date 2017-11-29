81c75c00ecf08ae4217a4b32f9c7487648d85363
Raúl Arellano <raul338@elhacker.net>
2017-11-29 12:06:23 -0300

    Save schema from current connection

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,13 +1,16 @@
 <?php
 use Cake\Core\Configure;
+use Cake\Event\Event;
 use Cake\Event\EventManager;
 use Schema\Shell\Task\SchemaSaveTask;
 
 $configKey = 'Schema.autoSaveSchemaAfterMigrate';
 
 if (!Configure::check($configKey) || Configure::read($configKey) === true) {
-    EventManager::instance()->on('Migration.afterMigrate', function () {
+    EventManager::instance()->on('Migration.afterMigrate', function (Event $event) {
         $task = new SchemaSaveTask;
+        $input = $event->getSubject()->getManager()->getInput();
+        $task->params['connection'] = $input->getOption('connection');
         $task->interactive = false;
         $task->initialize();
         $task->loadTasks();
