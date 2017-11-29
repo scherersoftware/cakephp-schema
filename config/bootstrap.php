9ccce94d8ab42bc3e4821783a96e8165b2cb31da
Raúl Arellano <raul338@elhacker.net>
2017-11-29 12:47:29 -0300

    Use save parameters instead of params property

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,11 +9,10 @@ $configKey = 'Schema.autoSaveSchemaAfterMigrate';
 if (!Configure::check($configKey) || Configure::read($configKey) === true) {
     EventManager::instance()->on('Migration.afterMigrate', function (Event $event) {
         $task = new SchemaSaveTask;
-        $input = $event->getSubject()->getManager()->getInput();
-        $task->params['connection'] = $input->getOption('connection');
         $task->interactive = false;
         $task->initialize();
         $task->loadTasks();
-        $task->save();
+        $input = $event->getSubject()->getManager()->getInput();
+        $task->save(['connection' => $input->getOption('connection')]);
     });
 }
